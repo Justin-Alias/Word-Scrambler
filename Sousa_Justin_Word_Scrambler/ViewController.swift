70054b8b762fb6fc8342da75e085acdb0aa3d907
Period Three <periodthree@imac-90.local>
2018-10-03 11:39:03 -0400

    Initial Commit

---
+++ Sousa_Justin_Word_Scrambler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sousa_Justin_Word_Scrambler
+//
+//  Created by Period Three on 2018-10-03.
+//  Copyright © 2018 Sousa_Justin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
